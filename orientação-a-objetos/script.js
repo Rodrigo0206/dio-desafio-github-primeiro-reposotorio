e7c75e0c6cffdae3a262ee2b00cb75f7d5ef918f
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2022-06-30 15:35:04 -0300

    desafio incial

---
+++ orientação-a-objetos/script.js
@@ -0,0 +1,14 @@
+// a função gets() é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print() para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+console.log('teste');
+let lines = gets().split('\n');
+var N = parseInt(lines.shift());
+
+for (let i = 1; i <= N; i++) {
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+	if (i % 2 ===  0  ) 
+          print(`${i}^2 = ${ N (5 < N < 2000) }`);
+}
+console.log('teste');
